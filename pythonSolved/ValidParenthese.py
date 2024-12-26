7f8b5354e77ad6ee9496cb75a175be2f81547fdc
Othmane Titebah <otitebah@e2r8p11.1337.ma>
2024-12-26 19:31:01 +0100

    + problem

---
+++ pythonSolved/ValidParenthese.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        check = []
+        i = 0
+        while i < len(s):
+            print(i, s[i], check)
+            if s[i] == "(" or s[i] == "{" or s[i] == "[":
+                check.append(s[i])
+            elif check[-1] == "(" and s[i] == ")":
+                    check.pop()
+            elif check[-1] == "{" and s[i] == "}":
+                check.pop()
+            elif check[-1] == "[" and s[i] == "]":
+                check.pop()
+            i += 1
+            
+        if len(check):
+            return False
+        return True
+                
+
+
+
+
